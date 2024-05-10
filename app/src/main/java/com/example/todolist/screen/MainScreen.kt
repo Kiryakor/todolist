17047bd75a28706cc8da8145833f2cfaa4234913
Kirill Kornyuschenkov <kakornyuschenkov@avito.ru>
2024-05-10 22:22:58 +0300

    support empty placeholder

--- app/src/main/java/com/example/todolist/screen/MainScreen.kt
+++ app/src/main/java/com/example/todolist/screen/MainScreen.kt
@@ -1,32 +1,44 @@
 package com.example.todolist.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.todolist.CreateTaskDialog
 import com.example.todolist.MainViewModel
+import com.example.todolist.R
 import com.example.todolist.Task
-import com.example.todolist.TaskView
 import com.example.todolist.ui.theme.TODOListTheme
+import com.example.todolist.view.CreateTaskDialog
+import com.example.todolist.view.TaskView
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
@@ -48,20 +60,27 @@ fun MainScreen(viewModel: MainViewModel) {
         )
         if (viewModel.tasks.isEmpty()) {
             Column(
+                verticalArrangement = Arrangement.Center,
                 modifier = Modifier
                     .verticalScroll(rememberScrollState())
                     .weight(weight = 1f, fill = true)
             ) {
-                Spacer(modifier = Modifier.weight(weight = 1f, fill = true))
-                Text(
-                    text = "У вас еще нет добавленных задач",
-                    style = TextStyle(fontSize = 25.sp),
-                    textAlign = TextAlign.Center,
+                Box(
+                    contentAlignment = Alignment.Center,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp)
-                )
-                Spacer(modifier = Modifier.weight(weight = 1f, fill = true))
+                        .fillMaxSize()
+                        .padding(all = 32.dp)
+                        .clip(RoundedCornerShape(30.dp))
+                        .background(Color(0xFFEDEDED))
+                ) {
+                    Text(
+                        text = "У вас еще нет добавленных задач",
+                        style = TextStyle(fontSize = 16.sp),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(all = 32.dp)
+                    )
+                }
             }
         } else {
             Column(
