b4ebe0f8c76e3f9f7189b726108f009315d7ef42
Kirill Kornyuschenkov <kakornyuschenkov@avito.ru>
2024-05-18 12:22:55 +0300

    version 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.todolist"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.3"
+        versionCode = 8
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
